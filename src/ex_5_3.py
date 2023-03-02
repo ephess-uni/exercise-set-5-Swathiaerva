5b78c5736ba6250168b5fdec463f302781784b91
Swathiaerva <123262539+Swathiaerva@users.noreply.github.com>
2023-03-02 23:13:46 +0530

    Update ex_5_3.py

--- src/ex_5_3.py
+++ src/ex_5_3.py
@@ -1,21 +1,3 @@
-""" ex_5_3.py
-This module contains an entry point that:
-
-- creates a CLi that accepts an input file of data to be processed
-- shifts and scales the data to a mean of 0 and a standard deviation 1
-- writes the file to the output file argument
-"""
-import numpy as np
-from argparse import ArgumentParser
-
-if __name__ == "__main__":
-    # Create your argument parser object here.
-    # Collect the filename arguments from the command line
-    # Rewrite your 5_3 logic here so that it utilizes the arguments passed from the command line.
-
-    # Tests will run your command using a system call.
-    # To test your program with arguments, run it from the command line
-    # (see README.md for more details)
     
 """ ex_5_3.py
 This module contains an entry point that:
@@ -40,8 +22,8 @@ if __name__ == "__main__":
     # (see README.md for more details)
     import os
     parser = ArgumentParser(description='This program applies a standard scale transform to the data in infile and writes it to outfile.')
-    parser.add_argument('infile',help='provide  file path')
-    parser.add_argument('outfile',help='provide file path')
+    parser.add_argument('infile')
+    parser.add_argument('outfile')
     args = parser.parse_args()
     data = np.loadtxt(args.infile)
     normalize = (data - data.mean(axis=0)) / data.std(axis=0)
